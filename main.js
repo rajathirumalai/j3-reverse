dd82a289dd0cc4f0d74a790358697dee5d2ecfa9
Mahilampooraja <112690153+rajathirumalai@users.noreply.github.com>
2022-12-15 12:33:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 let n = 5; 
 let b = "";
-
-for (i=1;i<=n;i++) {
-  for (j=1;j<=n-i+1;j++) {
-    b+=j;
+for(i=1;i<=n;i++) {
+  for(j=1;j<=n-i+1;j++) {
+    document.write(j+b)
   }
-  b+="\n";
+  document.write("<br>")
 }
-console.log(b);
+
